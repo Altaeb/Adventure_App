8b449156dbf52199ecdce0d5a2738d6077e324ac
aftiyah <abd2010ly@gmail.com>
2020-04-08 21:04:25 +0200

    Add Start Over Button

--- TheTiger/ViewController.swift
+++ TheTiger/ViewController.swift
@@ -11,6 +11,16 @@ import UIKit
 // MARK: - ViewController: UIViewController
 
 class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Start Over", style: .plain, target: self, action: #selector(startOver))
+    }
 
+    @objc func startOver() {
+        if let navigationController = navigationController {
+            navigationController.popToRootViewController(animated: true)
+        }
+    }
 }
 
